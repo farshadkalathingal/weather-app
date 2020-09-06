22726102ab8c8684302fcde9f3c1658778529dfc
Shad <farshadkalathingal@gmail.com>
2020-09-06 19:36:30 +0530

    Pressure, Time, Humidity

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+console.log("Working")
+
+// let city = localStorage.getItem('city')
+
+// if (city == null) {
+//     formSubmit('London')
+// } else {
+//     formSubmit(city)
+// }
+
+// let form = document.getElementById('search-form');
+
+// form.addEventListener('submit', function() {
+//     let city = document.getElementById('city').value;
+//     localStorage.setItem('city', city);
+// })
+
+// function formSubmit(city) {
+//     let form = document.getElementById('search-form');
+//     document.getElementById('city').value = city;
+//     form.submit();
+// }
