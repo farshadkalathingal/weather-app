2959b5c8ae4990dd2ae286aed1eea54e0e4a5d4e
Shad <farshadkalathingal@gmail.com>
2020-09-07 16:01:49 +0530

    Default city, error pages

--- static/js/script.js
+++ static/js/script.js
@@ -18,17 +18,22 @@ for (i = 0; i < acc.length; i++) {
 // let city = localStorage.getItem('city')
 
 // if (city == null) {
-//     formSubmit('London')
-// } else {
-//     formSubmit(city)
+//     city = "Malappuram"
 // }
 
 // let form = document.getElementById('search-form');
 
 // form.addEventListener('submit', function() {
 //     let city = document.getElementById('city').value;
+//     if (city == null) {
+//       city = 'Malappuram'
+//     }
 //     localStorage.setItem('city', city);
 // })
+
+// window.onload = function() {
+//   formSubmit(city)
+// }
 
 // function formSubmit(city) {
 //     let form = document.getElementById('search-form');
